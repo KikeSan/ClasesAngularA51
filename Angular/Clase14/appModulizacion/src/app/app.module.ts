7ff4cabe2c272c434e6e3c651907bb646ccc087e
Kike Sanchez <kikin1601@hotmail.com>
2019-09-10 22:01:11 -0500

    Modulizacion

---
+++ Angular/Clase14/appModulizacion/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
+import {RouterModule, Routes} from '@angular/router'
+import { ListadoComponent as ListadoLibrosComponent } from './libros/listado/listado.component';
+import { LibrosModule } from './libros/libros.module';
+
+const rutas:Routes = [
+  {path:'', component:LoginComponent},
+  {path:'home', component:HomeComponent},
+
+  {path:'libros', children:[
+    {path:'', component:ListadoLibrosComponent},
+  ]},
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(rutas),
+    LibrosModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
