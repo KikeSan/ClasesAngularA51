9426d8e909d6f58f5ed1342893ad42c8f207b41d
kikesan <kikin1601@hotmail.com>
2019-09-17 09:32:15 -0500

    cambios en pipe

--- Angular/Clase14/appPipes/src/app/filtros.pipe.ts
+++ Angular/Clase14/appPipes/src/app/filtros.pipe.ts
@@ -6,7 +6,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FiltrosPipe implements PipeTransform {
 
   transform(value: Array<any>, busqueda:string): any {
-    //const busquedaAutor = "Mario"
     const nuevoArreglo = value.filter(item=>
       item.autor.toUpperCase().indexOf(busqueda.toUpperCase()) > -1 || item.titulo.toUpperCase().indexOf(busqueda.toUpperCase()) > -1 || item.sinopsis.toUpperCase().indexOf(busqueda.toUpperCase()) > -1
     ) 
